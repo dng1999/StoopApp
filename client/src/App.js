f104bc2e61e8731171ba441c43a6f10a642a172d
Dorothy Ng <dorothyng1999@gmail.com>
2021-03-28 17:44:29 -0400

    passing info from pyflask to react dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({token}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{token}></h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ load_dotenv()# Set up the app
 app = Flask(__name__, template_folder='../client/public')
 
 def index():
-    return render_template('index.html')
+    return render_template('index.html', testToken = 'STOOPAPP TEST')
 
 if __name__ == '__main__':
     app.run(port=(os.getenv('PORT') if os.getenv('PORT') else 8000), debug=False)
