1fb27afb1412d526966c0de004857b9a55450fe2
Dorothy Ng <dorothyng1999@gmail.com>
2021-03-28 20:18:18 -0400

    rearrange files to follow tutorial better

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from dotenv import load_dotenv
+
 import os
 
-load_dotenv()# Set up the app
+load_dotenv()
 
-app = Flask(__name__, template_folder='../client/public')
+app = Flask(__name__, static_folder='../client/build/', static_url_path='/')
 
+@app.route('/')
 def index():
-    return render_template('index.html', testToken = 'STOOPAPP TEST')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(port=(os.getenv('PORT') if os.getenv('PORT') else 8000), debug=False)
